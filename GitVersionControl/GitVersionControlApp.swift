41c6c373aa091519b33e254e2bef8a643e838dd6
dazhi <dazhi1@sohu.com>
2024-03-31 15:13:36 +0800

    Initial Commit

---
+++ GitVersionControl/GitVersionControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitVersionControlApp.swift
+//  GitVersionControl
+//
+//  Created by 宋成志 on 2024/3/31.
+//
+
+import SwiftUI
+
+@main
+struct GitVersionControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
